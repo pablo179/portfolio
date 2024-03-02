0a014d88c2956d046c4ec556fc3b57b5e67bbb09
pablo179 <cm.pablo.prog@gmail.com>
2024-03-02 13:21:16 -0600

    reset portfolio

---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+export default function Home() {
+  return (
+    <div>
+      <h1
+        className="font-bold
+      text-3xl
+      text-center
+      text-blue-600
+      "
+      >
+        Main page
+      </h1>
+    </div>
+  );
+}
